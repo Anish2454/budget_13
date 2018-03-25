2370d2e3564c213694f60438df485c9ba1db1673
anish2454 <shenoy.anish.227@gmail.com>
2018-03-25 19:07:22 -0400

    added hw

--- index.js
+++ index.js
@@ -5,28 +5,45 @@
    2018-03-26
 **/
 
-
-
 var overviewData = {"1948": {"revenue": 312, "spending": 224, "total": 88.6, "total%": 4.5, "GDP": 1.97},
-		    "1942": {"revenue": 250, "spending": 817, "total": -568, "total%": -29.6, "GDP": 1.92}};
+		    "1942": {"revenue": 160, "spending": 390, "total": -227, "total%": -29.6, "GDP": 1.92}};
 
 var createYear = function(year){
+    var svg = document.getElementById("svg");
+
+    //CLEAR SVG
+    var firstChild = svg.firstChild;
+    while(firstChild){
+      console.log("Clearing: ");
+      console.log(firstChild);
+      svg.removeChild(firstChild);
+      firstChild = svg.firstChild;
+    }
+
     var yearHeader = document.getElementById("year");
-    yearHeader.innerHTML += year;
+    yearHeader.innerHTML = "United States Federal Budget for " + year;
     generateCircles(year);
 }
 
 var generateCircles = function(year){
+    //var lastCircleFinalX = 0;
     var yearData = overviewData[year];
     var sizes = [yearData["revenue"]/3, yearData["spending"]/3]
+    console.log(sizes);
     var colors = ["Green", "Red"];
     var svg = d3.select("svg");
-    
-    
     svg.selectAll("circle")
 	.data(sizes)
 	.enter().append("circle")
-	.attr("cx", function(d, i){ return 50 + (d) + (3* d * i) })
+	.attr("cx",
+    function(d, i){
+      if(i == 0){
+        return 200;
+      }
+      else{
+        return 550;
+      }
+    })
 	.attr("cy", 150)
 	.attr("r", 0)
 	.transition().duration(function(d,i){ return (i+1) * 2500; }).attr("r", function(d){ return d; })
@@ -35,14 +52,18 @@ var generateCircles = function(year){
     svg.selectAll("text")
 	.data(sizes)
     	.enter().append("text")
-	.attr("x", function(d, i){ return (d - 20) + (3 * d * i) })
+	.attr("x", function(d, i){
+    if(i == 0){
+      return 120;
+    }
+    else{
+      return 470;
+    }
+  })
 	.attr("y", 300)
 	.attr("font-family", "Garamond")
 	.attr("font-size", "20px")
 	.transition().delay(function(d,i){ return (i+1) * 2500; }).text(function(d){ return d*3 + " Billion Dollars"; });
-
-
-    
 
 };
 
@@ -53,42 +74,36 @@ var makeBar = function(){
     var totals =[y42["total"],y48["total"]]
 
     var chart = d3.select(".chart");
-   
-    
+
+
     chart.selectAll("div")
 	.data(totals)
 	.enter().append("div")
 	.transition().duration(2500).style("width",function(d){
 	    console.log(d);
 	    return Math.abs(d) + "px";})
-	
+
 	.style("background-color",function(d,i){
 	    if ( d < 0 ) { return "red";}
 	    else { return "green"; }} )
 	.text(function(d,i) {
-	    if ( i == 0 ) {return "1942: " + d + " Billion" ;} 
+	    if ( i == 0 ) {return "1942: " + d + " Billion" ;}
 	    else { return "1948: " + d +" Billion" ; }});
 
 };
-makeBar();
-createYear("1948");
-
-
-
 var yr = true;
 var changeYear = function(e){
     if(yr){
-	createYear("1942");
-	yr = !yr;
-	
+      createYear("1942");
+      yr = !yr;
     }
     else{
-	createYear("1948");
-	yr = !yr;
-	
+      createYear("1948");
+      yr = !yr;
     }
 }
 
 var buton = document.getElementById("switch");
 buton.addEventListener("click",changeYear);
-
+makeBar();
+createYear("1948");
